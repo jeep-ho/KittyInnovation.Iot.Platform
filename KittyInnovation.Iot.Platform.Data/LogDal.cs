8abdd089a83eb1cc1466150c3ecce68457f5a210
jeep-ho <jeep-ho@hotmail.com>
2017-09-12 13:44:26 -0700

    add log

---
+++ KittyInnovation.Iot.Platform.Data/LogDal.cs
@@ -0,0 +1,34 @@
+﻿using KittyInnovation.Iot.Platform.Domain.Entities;
+using NHibernate;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KittyInnovation.Iot.Platform.Data
+{
+    public class LogDal
+    {
+
+        public void Add(Log entity)
+        {
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                session.SaveOrUpdate(entity);
+            }
+        }
+
+        public Log GetLog(int id)
+        {
+            using (var session = NHibernateHelper.OpenSession())
+            {
+                var log = session.Get<Log>(id);
+                NHibernateUtil.Initialize(log.User);
+                return log;
+            }
+            //var session = NHibernateHelper.OpenSession();
+            //return session.Get<Log>(id);
+        }
+    }
+}
